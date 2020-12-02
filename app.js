dbd21cc377737dee45914399af2aac3e5377a16d
Henry Vu <henryvu25@gmail.com>
2020-12-02 15:31:58 -0800

    updated for env variable

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const app = express()
 
 
 var port = process.env.PORT || 3000
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/', (req, res) => res.send(process.env))
 app.listen(port, () => console.log("server running on port "+ port))
