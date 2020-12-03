f8786730cba6c22b578955ff0adb8b086b9f3397
Henry Vu <henryvu25@gmail.com>
2020-12-02 17:41:55 -0800

    change to port

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const app = express()
 
 
 var port = process.env.PORT || 3000
-app.get('/', (req, res) => res.send('App Setting: ' + process.env.APPSETTING_MyAppSettings + 'Connection String: ' + process.env.CUSTOMCONNSTR_MyConnectionString))
+app.get('/', (req, res) => res.send('port: ' + process.env.PORT))
 app.listen(port, () => console.log("server running on port "+ port))
